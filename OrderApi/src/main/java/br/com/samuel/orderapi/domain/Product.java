856503eb7c77e5baac56f64985462a2528a60be2
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-07-11 23:16:20 -0300

    implementation product class in order

---
+++ OrderApi/src/main/java/br/com/samuel/orderapi/domain/Product.java
@@ -0,0 +1,53 @@
+package br.com.samuel.orderapi.domain;
+
+import java.io.Serializable;
+
+
+public class Product implements Serializable {
+    private Long id_product;
+    private String name;
+    private String description;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(Long id_product, String name, String description, Double price) {
+        this.id_product = id_product;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId_product() {
+        return id_product;
+    }
+
+    public void setId_product(Long id_product) {
+        this.id_product = id_product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
