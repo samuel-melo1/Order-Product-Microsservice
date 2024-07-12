1c1723bd6ea73733eeaeef4025eb6dd1d562fc6c
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-07-11 23:16:38 -0300

    implementation feign of product

---
+++ OrderApi/src/main/java/br/com/samuel/orderapi/client/ProductFeign.java
@@ -0,0 +1,19 @@
+package br.com.samuel.orderapi.client;
+
+import br.com.samuel.orderapi.domain.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@FeignClient(name = "product-api", url = "http://localhost:8082")
+public interface ProductFeign {
+
+    @GetMapping("/api/v1/products")
+    ResponseEntity<List<Product>> findAll();
+    @GetMapping(value = "/api/v1/{id}")
+    public ResponseEntity<Optional<Product>> findById(@PathVariable("id") Long id);
+}
